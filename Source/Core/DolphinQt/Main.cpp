888f7b9c9155429525f8994ab41b22ab4aa507a4
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-11-19 20:53:21 -0500

    force set to dark mode

--- Source/Core/DolphinQt/Main.cpp
+++ Source/Core/DolphinQt/Main.cpp
@@ -248,6 +248,7 @@ int main(int argc, char* argv[])
 
     Settings::Instance().InitDefaultPalette();
     Settings::Instance().UpdateSystemDark();
+    Settings::Instance().SetSystemDark(true);  // sets the dark style
     Settings::Instance().ApplyStyle();
 
     MainWindow win{std::move(boot), static_cast<const char*>(options.get("movie"))};
