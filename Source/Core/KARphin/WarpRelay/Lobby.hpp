def0d5eeac52a3f241861c77c9f7773c38821058
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2025-06-01 23:00:28 -0400

    added lobby metadata

--- Source/Core/KARphin/WarpRelay/Lobby.hpp
+++ Source/Core/KARphin/WarpRelay/Lobby.hpp
@@ -6,11 +6,20 @@
 
 namespace KAR::Lobby
 {
+  //defines a lobby type
+  enum class LobbyType
+  {
+    Public = 0, //the default state of a lobby
+  };
+
 	//defines a lobby
 	struct Lobby
 	{
     uint8_t playerCount = 0; //the total number of players that are in the match
-    std::string gameID = ""; //stores the game ID
+    std::string gameID = "",  // stores the game ID
+        hostCode_IP = "",     // stores the host code/IP used to link to the lobby
+        password = "",        // the password used to enter a lobby
+        hostUsername = ""; //the user that was the host of the lobby
     std::string playerDisplayNames[4] = {"", "", "", ""}; //stores the player names
     std::string playerIconURLs[4] = {"", "", "", ""};  // stores the player urls
 	};
