d0af8fced774cdb8d8a6d81761cccf018fe8fe4c
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2025-08-23 13:20:58 -0400

    compressed change log

--- Source/Core/KARphin/ChangeLog.hpp
+++ Source/Core/KARphin/ChangeLog.hpp
@@ -3,22 +3,13 @@
 //defines the change log for KAR updates
 
 
-// 4.1.12
-#define KARPHIN_CHANGE_LOG_4_1_12                                                                  \
-  "<h3> 4.1.12 - Minor Changes <h3>"                                                               \
-  "<p>If the ROM disables memory cards, it will now disable the KWQI option for downloading memory cards.</p>"
-
-// 4.1.11
-#define KARPHIN_CHANGE_LOG_4_1_11                                                                \
-  "<h3> 4.1.11 - Minor Changes <h3>"                                                               \
-  "<p>Fixed bug with auto updater not checking major, minor, and hotfix priority.</p>"
-
-//4.1.10
-#define KARPHIN_CHANGE_LOG_4_1_10                                                                   \
-  "<h3> 4.1.10 - Minor Changes <h3>"                                                                \
+// 4.2.0
+#define KARPHIN_CHANGE_LOG_4_2_0 \
+  "<h3> 4.2.0 - Music Sync <h3>"\
+  "<p>Music will now sync for support ROMs. You can use the Music Sync menu. Requires the ROM to be extracted.</p>"\
+  "<h3> 4.2.0 - Minor Changes <h3>"\
+  "<p>If the ROM disables memory cards, it will now disable the KWQI option for downloading memory cards.</p>" \
+  "<p>Fixed bug with auto updater not checking major, minor, and hotfix priority.</p>" \
   "<p>About section now renders current change log and description changes.</p>"
 
-#define KARPHIN_CHANGE_LOG \
-KARPHIN_CHANGE_LOG_4_1_12                                                                        \
-KARPHIN_CHANGE_LOG_4_1_11 \
-KARPHIN_CHANGE_LOG_4_1_10
+#define KARPHIN_CHANGE_LOG KARPHIN_CHANGE_LOG_4_2_0
